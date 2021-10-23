9b5672e0f2a1e63656a6bec3da2f2938a6a20540
Raghav Acharya <18racharya@gmail.com>
2021-10-23 19:39:25 -0400

    Update wordchains.h

--- cx4010_PA_2/wordchains.h
+++ cx4010_PA_2/wordchains.h
@@ -18,7 +18,11 @@ void CreateMapping (char* filename,struct mapping string[WORDLIST]);
 void CreateAbJMartix (struct mapping string[WORDLIST],int matrix[WORDLIST][WORDLIST]);
 //Compare the a string to all the other strings return the difference 
 int CompareString(char* string1, char* string2);
+
 void printList(int list[]);
+//Adds a node onto a list
 void append(struct Node** head_ref, int nextWord);
+//Converts the found path of word indices into words and prints to terminal
 void printlList(struct mapping string[WORDLIST], int matrix[WORDLIST][WORDLIST], struct Node *node, int end);
+//Finds a path between the start and end word indices
 void pathFind(struct Node* path, int* adjMat, int start, int end);
